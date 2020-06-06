93f247af9b25d7599369d85f7fc166b2421b49a0
Meagan Rossi <meaganrossi@gmail.com>
2020-06-06 15:43:02 -0400

    whitman

--- _py/Spiders/spokane/spokane/items.py
+++ _py/Spiders/spokane/spokane/items.py
@@ -7,8 +7,26 @@
 
 import scrapy
 
+class SpokaneSpider(scrapy.Spider):
+    name = 'Spokane'
 
-class SpokaneItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+#     def remove_characters(self, value):
+#         return value.strip('\xa0')
+    
+    def start_requests(self):
+        yield SeleniumRequest(
+            url='https://www.spokanecounty.org/352/Inmate-Roster',
+            wait_time=3,
+            callback=self.parse
+        )
+
+    def parse(self, response):
+        products = response.xpath("//*")
+        for product in products:
+            yield {
+                'Date': product.xpath('//*[@id="aspnetForm"]/div[3]/h1').get(),
+                'County': product.xpath("/html/head/title").get()\
+#                 'State'
+#                 'Pop_Count': self.remove_characters(product.xpath("normalize-space(.//span[@class='itemStore']/text())").get()),
+#                 'Jail': product.xpath("normalize-space(.//div[@class='itemPrice  wide ']/text())").get()
+            }
